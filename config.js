d31ced77d1cee63d2b1dd2772f5dd0aad5609c89
Ward <ward.muse.dev@gmail.com>
2021-09-14 16:24:57 +0300

    First commit

---
+++ config.js
@@ -0,0 +1,33 @@
+// Create and export cinfiguration variables
+
+// container for all the environmets
+
+const envirnments = {};
+
+//  Staging (default) environment
+envirnments.staging = {
+  httpPort: 3001,
+  httpsPort: 3002,
+  envName: "staging",
+};
+
+// production env
+envirnments.production = {
+  httpPort: 5001,
+  httpsPort: 5002,
+  envName: "production",
+};
+
+//Determine which environment was passed as a command-line argument.
+const currentEnvironment =
+  typeof process.env.NODE_ENV == "string"
+    ? process.env.NODE_ENV.toLowerCase()
+    : "";
+
+// Check that the current environment is one of the envs. above , if not , default to staging
+const environmentToExport =
+  typeof envirnments[currentEnvironment] == "object"
+    ? envirnments[currentEnvironment]
+    : envirnments.staging;
+
+module.exports = environmentToExport;
---
+++ index.js
@@ -0,0 +1,125 @@
+const http = require("http");
+const https = require("https");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+const config = require("./config");
+const fs = require("fs");
+
+// Instantiate the HTTP server
+const httpServer = http.createServer((req, res) => {
+  unifiedServer(req, res);
+});
+
+//dynamically set the port for what ever we set the config to.
+httpServer.listen(config.httpPort, () => {
+  console.log(`listening on ${config.httpPort} in ${config.envName} mode`);
+});
+
+let httpsSeverOption = {
+  ket: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem"),
+};
+//dynamically set the port for what ever we set the config to on the Https server.
+
+const httpsServer = https.createServer(httpsSeverOption, (req, res) => {
+  unifiedServer(req, res);
+});
+
+// Instantiate the HTTPS server
+httpsServer.listen(config.httpsPort, () => {
+  console.log(`listening on ${config.httpsPort} in ${config.envName} mode`);
+});
+
+const unifiedServer = (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Get query string as an Object
+
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP  method (Like Get Post or Delete etc..)
+  const method = req.method.toLowerCase();
+
+  const headers = req.headers;
+
+  // payLoad(Stream of Data)
+  //Large Data received as small amount at a time
+  // after the data emitts the little pieces of data
+  //the buffer will have it as a long String
+
+  let decoder = new StringDecoder("utf-8");
+  // buffer = placehoder for a string
+  let buffer = "";
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    // choose the handler if exist , otherwise go to not found.
+
+    const chooseHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    // construct the data object to send the handler
+
+    const data = {
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer,
+    };
+
+    //Route the requist to the handler specified in the router
+    chooseHandler(data, (statusCode, payload) => {
+      // Use the statusCode called back by the handler , ot default to 200
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
+      // Use the payload called by the handler , or default to an empty object
+      payload = typeof payload == "object" ? payload : {};
+
+      //Convert the Payload  to String
+
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.setHeader("Contant-Type", "application/json");
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log("this is the response : " + statusCode, payloadString);
+    });
+  });
+};
+
+// the handlers
+let handlers = {};
+
+handlers.ping = (data, callback) => {
+  callback(200);
+};
+
+// // sample handler
+handlers.sample = (data, callback) => {
+  // Call back http status code , and a payload object.
+  callback(406, { name: "sample handler" });
+};
+// Not found handler
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+// requist router
+
+let router = {
+  sample: handlers.sample,
+
+  // we don't have to define the not found handler in the router
+
+  ping: handlers.ping,
+};
